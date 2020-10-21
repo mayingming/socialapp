a711988b88ae15a74ca5759732b6af86f763a600
mayingming <mayingming1202@gmail.com>
2020-10-22 09:50:28 +1100

    Update config.js

--- util/config.js
+++ util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: "AIzaSyA20cRqEnyT1gF8bW8Bkf93byfCJ2XWFmY",
-  authDomain: "melbournesocialapp.firebaseapp.com",
-  databaseURL: "https://melbournesocialapp.firebaseio.com",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
   projectId: "melbournesocialapp",
-  storageBucket: "melbournesocialapp.appspot.com",
-  messagingSenderId: "659502776744",
-  appId: "1:659502776744:web:f52e60102adc456aeb5d9d",
-  measurementId: "G-7QNEZ958YG",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
